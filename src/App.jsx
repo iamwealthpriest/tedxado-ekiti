65f2076995d7cc025a485643d0d6e2986b5bb912
Doreen John-Opara <doraopera589@gmail.com>
2025-08-01 20:18:38 +0100

    Resolve merge conflict: Keep both Toaster and ScrollToTop components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Team from "./pages/Team";
@@ -12,13 +13,14 @@ function App() {
   return (
     <Router>
       <Toaster position="top-right" />
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/team" element={<Team />} />
         <Route path="/speakers" element={<Speakers />} />
-        <Route path="/shop" element={<Shop />} /> 
+        <Route path="/shop" element={<Shop />} />
       </Routes>
       <Footer />
     </Router>
